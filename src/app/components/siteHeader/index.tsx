1ddc610a659c525c8775d0bd2d37d222777227cb
Dave Welsh <dave@qccareerschool.com>
2024-11-21 14:54:45 -0500

    Moved zIndex

--- src/app/components/siteHeader/index.tsx
+++ src/app/components/siteHeader/index.tsx
@@ -3,8 +3,8 @@ import { Subnav } from "./subnav";
 
 
 export const SiteHeader: FC = () => (
-  <div style={{ position: 'sticky', top: 0 }}>
-    <header style={{ background: 'red', padding: '2rem 0', zIndex: 100 }}>
+  <div style={{ position: 'sticky', top: 0, zIndex: 100 }}>
+    <header style={{ background: 'red', padding: '2rem 0' }}>
       Site Wide Header
     </header>
     <Subnav />
